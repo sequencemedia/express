12f114de901abb3f75065b13e0195e460b5df8de
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-07-29 18:52:00 +0100

    `chai`

--- test/src/index.spec.cjs
+++ test/src/index.spec.cjs
@@ -1,8 +1,4 @@
 const {
-  expect
-} = require('chai')
-
-const {
   getRequestSocket,
   isRequestSocketEncrypted,
 
@@ -24,38 +20,43 @@ const {
   getRequestHeadersCookie
 } = require('@sequencemedia/express')
 
-describe('@sequencemedia/express', () => {
-  describe('`getRequestSocket`', () => it('is a function', () => expect(getRequestSocket).to.be.a('function')))
+import('chai')
+  .then(({
+    expect
+  }) => {
+    describe('@sequencemedia/express', () => {
+      describe('`getRequestSocket`', () => it('is a function', () => expect(getRequestSocket).to.be.a('function')))
 
-  describe('`isRequestSocketEncrypted`', () => it('is a function', () => expect(isRequestSocketEncrypted).to.be.a('function')))
+      describe('`isRequestSocketEncrypted`', () => it('is a function', () => expect(isRequestSocketEncrypted).to.be.a('function')))
 
-  describe('`isRequestSecure`', () => it('is a function', () => expect(isRequestSecure).to.be.a('function')))
+      describe('`isRequestSecure`', () => it('is a function', () => expect(isRequestSecure).to.be.a('function')))
 
-  describe('`getRequestQuery`', () => it('is a function', () => expect(getRequestQuery).to.be.a('function')))
+      describe('`getRequestQuery`', () => it('is a function', () => expect(getRequestQuery).to.be.a('function')))
 
-  describe('`getRequestParams`', () => it('is a function', () => expect(getRequestParams).to.be.a('function')))
+      describe('`getRequestParams`', () => it('is a function', () => expect(getRequestParams).to.be.a('function')))
 
-  describe('`getRequestBaseUrl`', () => it('is a function', () => expect(getRequestBaseUrl).to.be.a('function')))
+      describe('`getRequestBaseUrl`', () => it('is a function', () => expect(getRequestBaseUrl).to.be.a('function')))
 
-  describe('`getRequestUrl`', () => it('is a function', () => expect(getRequestUrl).to.be.a('function')))
+      describe('`getRequestUrl`', () => it('is a function', () => expect(getRequestUrl).to.be.a('function')))
 
-  describe('`getRequestOriginalUrl`', () => it('is a function', () => expect(getRequestOriginalUrl).to.be.a('function')))
+      describe('`getRequestOriginalUrl`', () => it('is a function', () => expect(getRequestOriginalUrl).to.be.a('function')))
 
-  describe('`getRequestHeaders`', () => it('is a function', () => expect(getRequestHeaders).to.be.a('function')))
+      describe('`getRequestHeaders`', () => it('is a function', () => expect(getRequestHeaders).to.be.a('function')))
 
-  describe('`getRequestHeadersOrigin`', () => it('is a function', () => expect(getRequestHeadersOrigin).to.be.a('function')))
+      describe('`getRequestHeadersOrigin`', () => it('is a function', () => expect(getRequestHeadersOrigin).to.be.a('function')))
 
-  describe('`getRequestHeadersHost`', () => it('is a function', () => expect(getRequestHeadersHost).to.be.a('function')))
+      describe('`getRequestHeadersHost`', () => it('is a function', () => expect(getRequestHeadersHost).to.be.a('function')))
 
-  describe('`getRequestHeadersConnection`', () => it('is a function', () => expect(getRequestHeadersConnection).to.be.a('function')))
+      describe('`getRequestHeadersConnection`', () => it('is a function', () => expect(getRequestHeadersConnection).to.be.a('function')))
 
-  describe('`getRequestHeadersUserAgent`', () => it('is a function', () => expect(getRequestHeadersUserAgent).to.be.a('function')))
+      describe('`getRequestHeadersUserAgent`', () => it('is a function', () => expect(getRequestHeadersUserAgent).to.be.a('function')))
 
-  describe('`getRequestHeadersAccept`', () => it('is a function', () => expect(getRequestHeadersAccept).to.be.a('function')))
+      describe('`getRequestHeadersAccept`', () => it('is a function', () => expect(getRequestHeadersAccept).to.be.a('function')))
 
-  describe('`getRequestHeadersAcceptEncoding`', () => it('is a function', () => expect(getRequestHeadersAcceptEncoding).to.be.a('function')))
+      describe('`getRequestHeadersAcceptEncoding`', () => it('is a function', () => expect(getRequestHeadersAcceptEncoding).to.be.a('function')))
 
-  describe('`getRequestHeadersAcceptLanguage`', () => it('is a function', () => expect(getRequestHeadersAcceptLanguage).to.be.a('function')))
+      describe('`getRequestHeadersAcceptLanguage`', () => it('is a function', () => expect(getRequestHeadersAcceptLanguage).to.be.a('function')))
 
-  describe('`getRequestHeadersCookie`', () => it('is a function', () => expect(getRequestHeadersCookie).to.be.a('function')))
-})
+      describe('`getRequestHeadersCookie`', () => it('is a function', () => expect(getRequestHeadersCookie).to.be.a('function')))
+    })
+  })
